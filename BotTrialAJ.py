1fb00f34defaba02b1c29822545e3de79d8ac608
ajk8703 <ajkim8703@gmail.com>
2018-01-11 12:53:17 -0500

    Just started projecttttttttt

---
+++ BotTrialAJ.py
@@ -0,0 +1,19 @@
+import discord
+import asyncio
+from discord.ext import commands
+bot = commands.Bot(command_prefix='!', description='Auyyyyyyyyyyyye')
+
+@bot.event
+
+async def on_ready():
+    print ("Logged in as")
+    print(bot.user.name)
+
+
+@bot.command()
+async def hello():
+    await bot.say("Hello")
+
+
+bot.run("NDAwNzA3NTQxODQ5NjA0MDk2.DTfj9w.fJ9ztMwA7NTFuiiyJXv0Tdvwzn4")
+
