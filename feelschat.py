17a90874442458778b07739302210466579d12b2
ajk8703 <ajkim8703@gmail.com>
2018-01-18 13:25:35 -0500

    Just started projecttttttttt

--- feelschat.py
+++ feelschat.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_message(message):
 
-        if "sad" in message.content:
+        if " sad" in message.content:
             await client.send_message(message.channel, "The feelsbar is open: https://i.ytimg.com/vi/BpX8E-kavmI/maxresdefault.jpg")
 
 @client.event
