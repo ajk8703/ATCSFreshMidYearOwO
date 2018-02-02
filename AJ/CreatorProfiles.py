4d430fcff6ff8b854c3780c914195eee87273ad4
ajk8703 <ajkim8703@gmail.com>
2018-02-01 20:56:27 -0500

    Just started projecttttttttt

--- AJ/CreatorProfiles.py
+++ AJ/CreatorProfiles.py
@@ -7,21 +7,21 @@ bot = commands.Bot(command_prefix = ".#")
 async def Owen():
     await bot.say("""
     Meet Creator #Owen!\n\n#Owen likes:%40s
-        %s%45s
-        %s%45s
-        %s%45s
+        %s%62s
+        %s%48s
+        %s%52s
         %s
         %s
         
-        """ % ("#Owen dislikes:", "Food", "Understuffed Burritos", "Computer", "Code not Working", "Marching Band", "When people get my name Wrong", "Airsoft", "to Game"))
+        """ % ("#Owen dislikes:", "Food", "Understuffed Burritos", "Computer", "Code not Working", "Marching Band", "When people get my name wrong", "Airsoft", "to Game"))
 
 @bot.command()
 async def Vignesh():
     await bot.say("""
     Meet Creator #Vignesh!\n\n#Vignesh likes:%40s
-        %s%45s
-        %s%45s
-        %s%45s
+        %s%39s
+        %s%48s
+        %s%13s
         %s
         %s
         
@@ -31,22 +31,24 @@ async def Vignesh():
 async def Amani():
     await bot.say("""
     Meet Creator #Amani!\n\n#Amani likes:%40s
-        %s%45s
-        %s%45s
-        %s%45s
+        %s%30s
+        %s%47s
+        %s%37s
         %s
         %s
         
-        """ % ("#Amani dislikes:", "like", "dislike", "like", "dislike", "like", "dislike", "like", "like"))
+        """ % ("#Amani dislikes:", "Video Games", "Bullies", "Comics", "Smelly People", "My Phone", "Homework", "Sports", "Family"))
+
+
 
 
 @bot.command()
 async def AJ():
     await bot.say("""
     Meet Creator #A.J.!\n\n#A.J. likes:%40s
-        %s%45s
-        %s%45s
-        %s%45s
+        %s%43s
+        %s%44s
+        %s%42s
         %s
         %s
         %s
@@ -58,4 +60,4 @@ async def AJ():
 
 
 
-bot.run("NDAwNzA3NTQxODQ5NjA0MDk2.DTfj9w.fJ9ztMwA7NTFuiiyJXv0Tdvwzn4")
+bot.run("Mzk5OTg1ODQ4NzIxOTk3ODM0.DTVDcA.7T0LiBlo5aaq7MQXWclQ6lJTzPw")
